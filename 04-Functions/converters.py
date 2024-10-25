2441154b6515534a804975b0a8ee1ab8e6ca8d96
Oleksandr Pospielov <sashapospielov@gmail.com>
2024-10-25 12:26:16 +0200

    4-section5_partially

--- 04-Functions/converters.py
+++ 04-Functions/converters.py
@@ -4,6 +4,15 @@ def m_to_cm(n):
 def cm_to_m(n):
     return n/100
 
+def cm_to_inch(n):
+    return n/2.54
+def feet_to_inch(n):
+    return n*12
+def inch_to_cm(n):
+    return n*2.54
+def feet_and_inch_to_cm(feet, inches):
+    inches += feet_to_inch(feet)
+    return inch_to_cm(inches)
 if __name__ == "__main__":
     # only execute when you run this module
     # so you can test the functions in this place
